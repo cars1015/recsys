b1cb16da2c52c86a768976b14e146338900ca791
cars1015 <xiongzhendaxi@gmail.com>
2023-05-29 12:45:01 +0900

    Doc2vec

--- Doc2_model.py
+++ Doc2_model.py
@@ -9,26 +9,22 @@ dir="/home/onishi/recommend/"+data+"/pro_sg/"
 
 df=pd.read_csv(dir+"train.csv")
 print(df.shape)
-df_test=pd.read_csv(dir+"validation_tr.csv")
+df_tr=pd.read_csv(dir+"validation_tr.csv")
 size=len(df['uid'].unique())
-print("size")
-print(size)
-size2=len(df_test['uid'].unique())
-#ここでtest_trも含めた
-df=pd.concat([df,df_test])
+size_tr=len(df_tr['uid'].unique())
+#
+df=pd.concat([df,df_tr])
 df['sid']=df['sid'].astype(str)
-
+#Consider users as sentence and items as words.
 documents = []
 sentence=df.groupby('uid')['sid'].apply(list).tolist()
 for user_id, group in df.groupby('uid'):
-    #ここおかしくないか？
     titles = group['sid'].tolist()
     document = TaggedDocument(words=titles, tags=[str(user_id)])
     documents.append(document)
 for file_number in [1]:
     model = Doc2Vec(dm=1,vector_size=1200, min_count=1, epochs=20, window=3200,dm_concat=0,ns_exponent=0,sample=0.00001)
     model.build_vocab(documents)
-    #Doc2Vecモデルの学習
     model.train(documents, total_examples=model.corpus_count, epochs=model.epochs)
     model_dir=data+"_doc2model"
     model.save('/home/onishi/recommend/'+model_dir+'/model_1200_3200_ep20_dm{}_con'.format(file_number))
